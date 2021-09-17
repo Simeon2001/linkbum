2bfec60655b52655e04e98fdeb496c28ae5db5ad
Simeon2001 <jesusanyasimeon@gmail.com>
2021-09-18 00:48:37 +0100

    new

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn linktree.wsgi --log-file -
--- linktree/settings.py
+++ linktree/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +133,6 @@ REST_FRAMEWORK = {
     'rest_framework.authentication.SessionAuthentication',
     'rest_framework.authentication.TokenAuthentication')
 }
+
+if os.getcwd()=='/app':
+    DEBUG=False
