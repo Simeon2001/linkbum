cf4216a94f83648edc4bcd34eccbbef1fa257f78
Ciscoquan <38753254+Simeon2001@users.noreply.github.com>
2021-09-18 01:02:06 +0100

    Delete lnk directory

--- lnk/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import profile,Social_Media,site_links,feedback
-# Register your models here.
-admin.site.register(profile)
-admin.site.register(Social_Media)
-admin.site.register(site_links)
-admin.site.register(feedback)
--- lnk/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LnkConfig(AppConfig):
-    name = 'lnk'
