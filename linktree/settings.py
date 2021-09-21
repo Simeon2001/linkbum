be9e26b272461832b91d216b3bea9719af0ef2ee
Simeon2001 <jesusanyasimeon@gmail.com>
2021-09-21 23:55:00 +0100

    'g'

--- linktree/settings.py
+++ linktree/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'name',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -145,3 +148,5 @@ STATICFILES_DIRS = (
 
 if os.getcwd()=='/app':
     DEBUG=False
+
+CORS_ORIGIN_ALLOW_ALL = True
