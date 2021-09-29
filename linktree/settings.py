1dcd37833e23977c516c2f962489841544d80c94
Simeon2001 <jesusanyasimeon@gmail.com>
2021-09-29 01:49:00 +0100

    'new'

--- linktree/settings.py
+++ linktree/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'linktree.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'libum',
+        'USER': 'ooniiogi',
+        'PASSWORD': '7fFWmTLL3KMbUNt2VIG2r1FM0pYkf3eO',
+        'HOST': 'postgres://ooniiogi:7fFWmTLL3KMbUNt2VIG2r1FM0pYkf3eO@surus.db.elephantsql.com/ooniiogi',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
