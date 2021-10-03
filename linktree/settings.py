17ded487ce1f579f31aa4441da1311eae5b7a209
Simeon2001 <jesusanyasimeon@gmail.com>
2021-10-03 04:03:21 +0100

    new

--- linktree/settings.py
+++ linktree/settings.py
@@ -136,7 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework.authentication.BasicAuthentication',
-    'rest_framework.authentication.SessionAuthentication',
     'rest_framework.authentication.TokenAuthentication')
 }
 
